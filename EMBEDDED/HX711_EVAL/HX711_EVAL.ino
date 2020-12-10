c15a3abbe2e92154a8d365fb14cee2729a48d8ab
grant-lewis <67376640+grant-lewis@users.noreply.github.com>
2020-12-11 00:36:22 +0800

    Add files via upload

---
+++ EMBEDDED/HX711_EVAL/HX711_EVAL.ino
@@ -0,0 +1,21 @@
+#include "HX711.h"
+
+#define calibration_factor -7050.0
+
+#define LOADCELL_DOUT_PIN  3
+#define LOADCELL_SCK_PIN  2
+
+HX711 scale;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 scale demo");
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(calibration_factor);
+  scale.tare();
+}
+
+void loop() {
+  Serial.print(scale.get_units(), 1);
+}
